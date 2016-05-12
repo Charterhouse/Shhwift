414db70f14e5baab327d1b3142ac60dcef795178
Mark Spanbroek <mark@spanbroek.net>
2016-05-12 17:08:41 +0200

    Introduced Shh class.

---
+++ Shhwift/Shh.swift
@@ -0,0 +1,3 @@
+public struct Shh {
+    public init() {}
+}
---
+++ ShhwiftTests/ShhSpec.swift
@@ -0,0 +1,11 @@
+import Quick
+import Nimble
+import Shhwift
+
+class ShhSpec: QuickSpec {
+    override func spec () {
+        it("can be created") {
+            _ = Shh()
+        }
+    }
+}
