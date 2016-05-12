cda76c3753e43b9dddedc8b6e6d24caed7d2c4af
Mark Spanbroek <mark@spanbroek.net>
2016-05-12 23:01:24 +0200

    HTTP mocking example with Mockingjay and Alamofire.

--- ShhwiftTests/ShhSpec.swift
+++ ShhwiftTests/ShhSpec.swift
@@ -1,11 +1,28 @@
 import Quick
 import Nimble
 import Shhwift
+import Mockingjay
+import Alamofire
 
 class ShhSpec: QuickSpec {
     override func spec () {
         it("can be created") {
             _ = Shh()
+        }
+
+        it("can mock http requests") {
+            let body = ["result": "42.0"]
+            self.stub(
+                http(.GET, uri: "http://some.ethereum.node:8545"),
+                builder: json(body)
+            )
+
+            waitUntil { done in
+                Alamofire.request(.GET, "http://some.ethereum.node:8545").responseJSON { response in
+                    expect(response.result.error).to(beNil())
+                    done()
+                }
+            }
         }
     }
 }
