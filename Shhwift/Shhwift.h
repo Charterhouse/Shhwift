3bbe475055728b14bf6419414abe609ddb5d48d7
Mark Spanbroek <mark@spanbroek.net>
2016-05-12 16:52:06 +0200

    Empty project.

---
+++ Shhwift/Shhwift.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shhwift.
+FOUNDATION_EXPORT double ShhwiftVersionNumber;
+
+//! Project version string for Shhwift.
+FOUNDATION_EXPORT const unsigned char ShhwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shhwift/PublicHeader.h>
+
+
