a7845437a2560d96ab775c5a7ed6361d66622570
Mark Spanbroek <mark@spanbroek.net>
2016-06-08 10:31:37 +0200

    Add call to Shh.version in demo app.

--- ShhwiftDemo/ViewController.swift
+++ ShhwiftDemo/ViewController.swift
@@ -1,8 +1,12 @@
 import UIKit
+import Shhwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        Shh(url: "http://localhost:8545").version { (version, error) in
+            print("version: \(version), error: \(error)")
+        }
     }
 }
