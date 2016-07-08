692c87e158aaada872006f1bce237349e46854b3
Mark Spanbroek <mark@spanbroek.net>
2016-07-08 14:20:42 +0200

    Add conversion from hexadecimal string to UInt.

--- Shhwift/UInt+HexString.swift
+++ Shhwift/UInt+HexString.swift
@@ -1,4 +1,16 @@
 extension UInt {
+
+    init?(hexString: String) {
+        guard hexString.hasPrefix("0x") else {
+            return nil
+        }
+
+        let hexStringWithoutPrefix = hexString
+            .substringFromIndex(hexString.startIndex.advancedBy(2))
+
+        self.init(hexStringWithoutPrefix, radix: 16)
+    }
+
     var asHexString: String {
         return "0x" + String(self, radix:16, uppercase: false)
     }
--- ShhwiftTests/UInt+HexStringSpec.swift
+++ ShhwiftTests/UInt+HexStringSpec.swift
@@ -7,5 +7,24 @@ class UIntHexStringSpec: QuickSpec {
         it("converts to hexadecimal string with prefix") {
             expect(UInt(1024).asHexString) == "0x400"
         }
+
+        it("can be constructed from a hex string") {
+            expect(UInt(hexString: "0x400")) == 1024
+        }
+
+        it("cannot be constructed with an empty string") {
+            let invalidString = ""
+            expect(UInt(hexString: invalidString)).to(beNil())
+        }
+
+        it("cannot be constructed with string containing non-hex characters") {
+            let invalidString = "0xZZ"
+            expect(UInt(hexString: invalidString)).to(beNil())
+        }
+
+        it("cannot be constructed with a string that lacks the 0x prefix") {
+            let invalidString = "--400"
+            expect(UInt(hexString: invalidString)).to(beNil())
+        }
     }
 }
