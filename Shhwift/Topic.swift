bef92ebf802af3e7212ae272c9967c98544c6f9e
Stefan van den Oord <stefan.van.den.oord@philips.com>
2016-07-12 16:54:17 +0200

    [Incorrect spec] Topics don't have to be 32 bytes

--- Shhwift/Topic.swift
+++ Shhwift/Topic.swift
@@ -3,10 +3,6 @@ public struct Topic: DataContainer {
     private let data: NSData
 
     public init?(_ data: NSData) {
-        guard data.length == 32 else {
-            return nil
-        }
-
         self.data = data
     }
 
--- ShhwiftTests/TopicSpec.swift
+++ ShhwiftTests/TopicSpec.swift
@@ -12,10 +12,10 @@ class TopicSpec: QuickSpec {
             expect(Topic(thirtyTwoBytesData)).toNot(beNil())
         }
 
-        it("cannot be constructed with data of different length") {
-            let invalidBytes = [UInt8](count: 50, repeatedValue: 42)
-            let invalidData = NSData(bytes: invalidBytes, length: invalidBytes.count)
-            expect(Topic(invalidData)).to(beNil())
+        it("can be constructed with data of different length") {
+            let bytes = [UInt8](count: 50, repeatedValue: 42)
+            let data = NSData(bytes: bytes, length: bytes.count)
+            expect(Topic(data)).toNot(beNil())
         }
 
         it("can be converted to data") {
